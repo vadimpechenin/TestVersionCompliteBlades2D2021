5f6aec16e344b770e6ea8818f57c77b81896b1d1
Vadim Pechenin <vadim.pechenin2015@gmail.com>
2021-07-22 12:40:29 +0400

    resultproxy_to_dict upgrade

--- db/supportFunctions.py
+++ db/supportFunctions.py
@@ -7,8 +7,13 @@ def resultproxy_to_dict(s2):
     d, a = {}, []
     for rowproxy in s2:
         # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
-        for column, value in rowproxy.items():
+        # for column, value in rowproxy.items():
+        # Для SQLAlchemy 1.4.17
+        k=0
+        for column in rowproxy._fields:
             # build up the dictionary
+            value = rowproxy._data[k]
             d = {**d, **{column: value}}
+            k+=1
         a.append(d)
     return a
